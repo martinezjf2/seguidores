35515fa8d0845915a0614f777f09db251da0ccda
Jeffrey Martinez <martinezjf2@gmail.com>
2023-01-18 01:19:42 -0500

    added a header for the website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+// import "./App.css";
 import React from "react";
 import { navbar as links } from "./data.js";
 import NavbarComponent from "./Navbar.js";
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 export default function Header() {
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Bienvenidos</Card.Title>
-        <Card.Text>Seguidores de Jesus</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+    <Card id="header">
+      <Card.Body id="card-body">
+        <Card.Title id="header-welcome" className="text-center">
+          Bienvenidos
+        </Card.Title>
+        <Card.Text id="header-group" className="text-center">
+          Seguidores de Jesus
+        </Card.Text>
       </Card.Body>
     </Card>
   );
