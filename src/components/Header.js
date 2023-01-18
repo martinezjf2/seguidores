0535af374a7162c01bbf52d35cd78cd200a5b627
Jeffrey Martinez <martinezjf2@gmail.com>
2023-01-18 17:16:43 -0500

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import groupImage from '../group.png';
-import joseph from '../joseph.png';
-
+import groupImage from "../group.png";
+import elmer from "../elmer.png";
+import fermina from "../fermina.png";
+import john from "../john.png";
+import jeffrey from "../jeffrey.png";
+import joseph from "../joseph.png";
 
 export default function Header() {
   return (
     <Card id="header">
-      <img src={groupImage} id="first-image"/>
-      <img src={groupImage} id="second-image"/>
+      <img src={groupImage} id="first-image" alt="group" />
+      <img src={elmer} id="second-image" alt="elmer" />
+      <img src={fermina} id="third-image" alt="fermina" />
+      <img src={john} id="fourth-image" alt="john" />
+      <img src={jeffrey} id="fifth-image" alt="jeffrey" />
+      <img src={joseph} id="sixth-image" alt="joseph" />
 
-      <Card.Body id="card-body">
+      <Card.Body id="card-body" style={{ animation: "fadein 10s" }}>
         <Card.Title id="header-welcome" className="text-center">
           Bienvenidos
         </Card.Title>
